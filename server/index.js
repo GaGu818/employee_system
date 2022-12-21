9b31d2ed4ff699faba6b7a04f866c3d0a053a3ff
GaGu818 <nicolexu0818@outlook.com>
2022-12-21 11:54:48 -0600

    backend set up

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+// basic configuration
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//ports
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
+
+app.get("/", (req, res) => {
+  res.send("main page");
+});
